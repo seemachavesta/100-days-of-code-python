77d730949304a6c5fee07b2af26e56304194f292
Seema Chavesta <seemachavesta@yahoo.com>
2024-12-10 18:51:31 -0800

    Initial commit

---
+++ movie_collection.py
@@ -0,0 +1,6 @@
+movie_collection = [
+    {'Title': 'Inception', 'Director': 'Christopher Nolan', 'Release Year': 2010},
+    {'Title': 'The Shawshank Redemption', 'Director': 'Frank Darabont', 'Release Year': 1994},
+    {'Title': 'The Godfather', 'Director': 'Francis Ford Coppola', 'Release Year': 1972},
+    {'Title': 'Pulp Fiction', 'Director': 'Quentin Tarantino', 'Release Year': 1994}
+]
