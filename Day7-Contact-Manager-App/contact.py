89aff96ff933a0f9403134ae65a8c823c12e6366
Seema Chavesta <seemachavesta@yahoo.com>
2025-02-28 12:44:11 -0800

    Fixed folder order by renaming them with leading zeros

--- Day7-Contact-Manager-App/contact.py
+++
@@ -1,9 +0,0 @@
-class Contact:
-  def __init__(self, name, phone_number, email, address):
-    self.name = name
-    self.phone_number = phone_number
-    self.email = email
-    self.address = address
-    
-  def __str__(self):
-    return f"Name: {self.name}, Phone Number: {self.phone_number}, Email: {self.email}, Address: {self.address}"
--- Day9-Library-System/book.py
+++
@@ -1,7 +0,0 @@
-from libraryItem import LibraryItem
-
-class Book(LibraryItem):
-  def __init__(self, title, author, publication_year, genre, status):
-    super().__init__(title, author, publication_year, status)
-    self.genre = genre
-    
